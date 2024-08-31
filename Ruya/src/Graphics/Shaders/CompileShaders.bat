631326b4a9ae11c58c5defabf63b506fc8d24b6f
Alperensahinn <aalperennsahinn@gmail.com>
2024-08-31 22:46:21 +0300

    clear init

--- Ruya/src/Graphics/Shaders/CompileShaders.bat
+++
@@ -1,7 +0,0 @@
-C:\VulkanSDK\1.3.290.0\Bin\glslc.exe VertexShader.vert -o vert.spv
-C:\VulkanSDK\1.3.290.0\Bin\glslc.exe FragmentShader.frag -o frag.spv
-C:\VulkanSDK\1.3.290.0\Bin\glslc.exe ComputeTest.comp -o ComputeTest.spv
-C:\VulkanSDK\1.3.290.0\Bin\glslc.exe GradientColor.comp -o GradientColor.spv
-C:\VulkanSDK\1.3.290.0\Bin\glslc.exe ColoredTriangle.vert -o ColoredTriangleVertexShader.spv
-C:\VulkanSDK\1.3.290.0\Bin\glslc.exe ColoredTriangle.frag -o ColoredTriangleFragmentShader.spv
-pause
